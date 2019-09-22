9ed7a7c185612f3f165a4fda7197357aa3dc7d4e
Huan-Yu Tseng <zxc1020305@gmail.com>
2019-09-22 22:37:45 +0800

    Using exist website pattern
    Using a exist website pattern

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mitch Tseng\'s Personal Website' });
 });
 
 module.exports = router;
